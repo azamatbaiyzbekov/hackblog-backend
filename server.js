be2788f0ad432fd2a53b8b5e8f99ff3c53f07ca7
Azamat Baiyzbekov <azamatbaiyzbekov@Azamats-MacBook-Pro.local>
2019-10-21 21:44:41 -0700

    updated

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const routes = require('./routes');
-const path = require('path');
+
 
 
 
@@ -51,15 +51,6 @@ app.use('/api/v1/users', routes.users);
 
 app.use('/api/v1/posts', routes.posts);
 
-
-//
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static( 'front-end/blog-app/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'front-end', 'blog-app', 'build', 'index.html'))
-    })
-}
 
 
 app.listen(PORT, () => {
